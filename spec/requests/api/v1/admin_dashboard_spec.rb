802a0df6d79872988a3892b6742a3825a0a08dcd
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-09-22 23:59:57 -0700

    Add dashboard route, controller, view, and specs for current trimester

---
+++ spec/requests/api/v1/admin_dashboard_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Dashboard', type: :request do
+  describe 'GET /dashboard' do
+    before do
+      # Create test data for the current and upcoming trimesters
+      # Define current_trimester
+      Trimester.create!(
+        term: 'Current term',
+        year: Date.today.year.to_s,
+        start_date: Date.today - 1.day,
+        end_date: Date.today + 2.months,
+        application_deadline: Date.today - 16.days
+      )
+      # Define upcoming_trimester
+      #  upcoming_trimester = Trimester.create!(
+    end
+
+    it 'returns a 200 OK status' do
+      # Send a GET request to the dashboard route
+      get '/dashboard'
+
+      # Check that the response status is 200 (OK)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'displays the current trimester' do
+      get '/dashboard'
+      expect(response.body).to include("Current term - #{Date.today.year}")
+    end
+
+    it 'displays links to the courses in the current trimester' do
+    end
+
+    it 'displays the upcoming trimester' do
+    end
+
+    it 'displays links to the courses in the upcoming trimester' do
+    end
+  end
+end
