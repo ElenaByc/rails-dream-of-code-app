5dd318301c147863dfb4fdea3721dbd1a21fa07a
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-10-01 00:52:30 -0700

    Populate dropdowns on new submission form

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,4 +3,8 @@ class Enrollment < ApplicationRecord
   belongs_to :student
   has_many :mentor_enrollment_assignments
   has_many :submissions
+
+  def student_name
+    "#{student.first_name} #{student.last_name}"
+  end
 end
--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,3 +1,7 @@
 class Mentor < ApplicationRecord
   has_many :submissions
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
