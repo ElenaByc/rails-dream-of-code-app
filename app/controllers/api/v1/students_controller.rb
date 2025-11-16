b19f01d6554c4e9aa5d01376ebf8bf25e1c72f38
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-11-15 17:55:23 -0800

    Implement dynamic API endpoint to create students

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -2,13 +2,24 @@ class Api::V1::StudentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    student = {
-      id: 123,
-      first_name: "Marge",
-      last_name: "Simpson",
-      email: "marge@simpsons.com"
-    }
+    student = Student.new(student_params)
 
-    render json: { student: student }, status: :created
+    if student.save
+      student_hash = {
+        id: student.id,
+        first_name: student.first_name,
+        last_name: student.last_name,
+        email: student.email
+      }
+      render json: { student: student_hash }, status: :created
+    else
+      render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
   end
 end
